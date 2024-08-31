b440024972bf422479ff1a7730d81b4c2d268384
codekamit <kamitstar@icloud.com>
2024-08-31 05:49:29 +0530

    initial setup1

---
+++ appmain/src/main/java/com/kamit/appmain/AppmainApplication.java
@@ -0,0 +1,13 @@
+package com.kamit.appmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmainApplication.class, args);
+	}
+
+}
---
+++ appmain/src/test/java/com/kamit/appmain/AppmainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamit.appmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
